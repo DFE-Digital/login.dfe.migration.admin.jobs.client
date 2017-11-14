cf47637dd86ca78d2094bc6072a287c4e2d468f1
Simon Bull <simon.bull.84@googlemail.com>
2017-11-14 16:58:44 +0000

    Capture old credentials on invite

--- test/sendInvite.test.js
+++ test/sendInvite.test.js
@@ -1,10 +1,13 @@
 jest.mock('kue');
 
 const connectionString = 'connection-string';
-const email = '';
-const firstName = '';
-const lastName = '';
+const email = 'user.one@unit.test';
+const firstName = 'user';
+const lastName = 'one';
 const services = [];
+const oldUsername = 'fsdjkhsf';
+const oldPassword = 'sfuinw';
+const oldSalt = 'fwfnejwnkf';
 
 describe('when sending an invitation job', () => {
   let createQueue;
@@ -38,32 +41,37 @@ describe('when sending an invitation job', () => {
   });
 
   it('then it should queue to the configured endpoint', async () => {
-    await client.sendInvite(email, firstName, lastName, services);
+    await client.sendInvite(email, firstName, lastName, services, oldUsername, oldPassword, oldSalt);
 
     expect(createQueue.mock.calls.length).toBe(1);
     expect(createQueue.mock.calls[0][0].redis).toBe('connection-string');
   });
 
   it('then it should create a migationinvite_v1 job', async () => {
-    await client.sendInvite(email, firstName, lastName, services);
+    await client.sendInvite(email, firstName, lastName, services, oldUsername, oldPassword, oldSalt);
 
     expect(queueCreate.mock.calls.length).toBe(1);
     expect(queueCreate.mock.calls[0][0]).toBe('migationinvite_v1');
   });
 
   it('then it should create the job with supplied data', async () => {
-    await client.sendInvite(email, firstName, lastName, services);
+    await client.sendInvite(email, firstName, lastName, services, oldUsername, oldPassword, oldSalt);
 
     expect(queueCreate.mock.calls[0][1]).toMatchObject({
       email,
       firstName,
       lastName,
       services,
+      oldCredentials: {
+      username: oldUsername,
+        password: oldPassword,
+        salt: oldSalt,
+    },
     });
   });
 
   it('then it should save the job', async () => {
-    await client.sendInvite(email, firstName, lastName, services);
+    await client.sendInvite(email, firstName, lastName, services, oldUsername, oldPassword, oldSalt);
 
     expect(jobSave.mock.calls.length).toBe(1);
   });
@@ -74,7 +82,7 @@ describe('when sending an invitation job', () => {
     });
 
     try {
-      await client.sendInvite(email, firstName, lastName, services);
+      await client.sendInvite(email, firstName, lastName, services, oldUsername, oldPassword, oldSalt);
       throw new Error('No error thrown');
     } catch (e) {
       expect(e.message).toBe('some error');
